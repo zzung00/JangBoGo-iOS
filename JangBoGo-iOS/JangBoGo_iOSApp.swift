d2a19d1d90bb5d79ecc1bfb218ad78e4d8fce542
zzung00 <lee759444@gmail.com>
2023-05-20 16:13:29 +0900

    Initial Commit

---
+++ JangBoGo-iOS/JangBoGo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JangBoGo_iOSApp.swift
+//  JangBoGo-iOS
+//
+//  Created by 이정현 on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct JangBoGo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
